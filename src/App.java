b2413cbe4432860c8eed7657abd7e59bf5932e26
DAM <sergiodiaz808@gmail.com>
2016-12-13 18:35:05 +0100

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class App extends JFrame{
+
+    private JPanel panel;
+    private JTextField textField1;
+    private JButton validarButton;
+    private JPasswordField passwordField1;
+
+    public static void main(String[] args) {
+        App app = new App();
+    }
+
+
+}
